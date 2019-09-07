42e33e193a1933eca5748d28733e6ef8e2284c84
Paul-Antoine <palevasseur@gmail.com>
2019-09-07 19:38:09 +0200

    small updates

--- index.ts
+++ index.ts
@@ -4,17 +4,19 @@ const AWS = require('aws-sdk');
 const express = require('express');
 
 const appConfig = {
-    phonesList: ['+33607666798', '+33643207293']
+    port: 4000,
+    topicArn: 'arn:aws:sns:eu-west-1:089938603528:school-notif-topic',
+    region: 'eu-west-1' // Ireland (Paris or Frankfurt don't have SMS)
 };
 
 const credentials = new AWS.SharedIniFileCredentials({profile: 'school-notif-account'});
 console.log('Use aws credentials:', credentials);
 AWS.config.credentials = credentials;
-AWS.config.region = 'eu-west-1'; // Ireland (Paris or Frankfurt don't have SMS)
+AWS.config.region = appConfig.region;
 
 const app = express();
 
-// serve static file, ex: http://localhost:4000/index.html || http://localhost:4000
+// serve static file, ex: http://localhost:4000/index.html || http://localhost:4000/index.html
 app.use(express.static(__dirname + '/web'));
 
 // sms api
@@ -28,12 +30,12 @@ app.get('/notif', (req, res) => {
         const sns = new AWS.SNS();
         const params = {
             Message: req.query.message,
-            TopicArn: 'arn:aws:sns:eu-west-1:089938603528:school-notif-topic'
+            TopicArn: appConfig.topicArn
         };
 
         const pub = sns.publish(params).promise();
         pub.then(data => {
-            console.log('' + new Date().toLocaleString() + ': Successful sent email, data=' + JSON.stringify(data) + ', params=' + JSON.stringify(params));
+            console.log('' + new Date().toLocaleString() + ': Successful sent notif, data=' + JSON.stringify(data) + ', params=' + JSON.stringify(params));
             const success = {
                 date: new Date().toLocaleString(),
                 action: 'email',
@@ -43,7 +45,7 @@ app.get('/notif', (req, res) => {
             };
             res.send(JSON.stringify(success));
         }).catch(err => {
-            console.log('' + new Date().toLocaleString() + ': FAILED to sent email, error=' + err + ', params=' + JSON.stringify(params), err.stack);
+            console.log('' + new Date().toLocaleString() + ': FAILED to sent notif, error=' + err + ', params=' + JSON.stringify(params), err.stack);
             const failed = {
                 date: new Date().toLocaleString(),
                 action: 'email',
@@ -57,18 +59,18 @@ app.get('/notif', (req, res) => {
         return pub;
     }
     catch (e) {
-        console.log('' + new Date().toLocaleString() + ': Send sms FAILED, message=' + req.query.message + ' to ' + JSON.stringify(appConfig.phonesList) + ', Error=', e);
+        console.log('' + new Date().toLocaleString() + ': Send notif FAILED, message=' + req.query.message + ', Error=', e);
     }
 });
 
 app.get('/log', (req, res) => {
     const pathLog = __dirname + '/school-notif.log';
     fs.existsSync(pathLog)
-        ? res.send('' + fs.readFileSync(pathLog)) // need to convert to string (avoid "download file")
+        ? res.send('<html><div><pre>' + fs.readFileSync(pathLog) + '</pre></div></html>') // need to send a string (avoid "download file")
         : res.send('Log file "' + pathLog + '" doesn\'t exist');
 });
 
-app.listen(4000, () => {
-    console.log('App listening on http://localhost:4000 / http://192.168.1.16:4000');
+app.listen(appConfig.port, () => {
+    console.log(`App listening on http://localhost:${appConfig.port} / http://192.168.1.16:${appConfig.port}`);
 });
 
